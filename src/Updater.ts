d8f674ecdf2b52e5879f9fab96ff1240266cb8ba
Jay Meistrich <jmeistrich@gmail.com>
2025-03-22 20:38:34 +0800

    Disable the auto updater for now, we can come back to it later

--- src/Updater.ts
+++ src/Updater.ts
@@ -1,35 +1,32 @@
-import { observe } from '@legendapp/state';
-import { AutoUpdaterModule } from './native/AutoUpdater';
-import { settings$ } from './settings/SettingsFile';
-
 export function initializeUpdater() {
   // Don't update in dev
   if (__DEV__) {
     return;
   }
 
-  let previousCheckUpdates = false;
-  observe(async () => {
-    const autoUpdateEnabled = settings$.general.autoUpdate.enabled.get();
-    const checkInterval = settings$.general.autoUpdate.checkInterval.get() * 3600; // Convert hours to seconds
+  // TODO: Implement auto-updater
+  //   let previousCheckUpdates = false;
+  //   observe(async () => {
+  //     const autoUpdateEnabled = settings$.general.autoUpdate.enabled.get();
+  //     const checkInterval = settings$.general.autoUpdate.checkInterval.get() * 3600; // Convert hours to seconds
 
-    try {
-      // Configure auto-update based on settings
-      await Promise.all([
-        AutoUpdaterModule.setAutomaticallyChecksForUpdates(autoUpdateEnabled),
-        autoUpdateEnabled
-          ? AutoUpdaterModule.setUpdateCheckInterval(checkInterval)
-          : Promise.resolve(true),
-      ]);
+  //     try {
+  //       // Configure auto-update based on settings
+  //       await Promise.all([
+  //         AutoUpdaterModule.setAutomaticallyChecksForUpdates(autoUpdateEnabled),
+  //         autoUpdateEnabled
+  //           ? AutoUpdaterModule.setUpdateCheckInterval(checkInterval)
+  //           : Promise.resolve(true),
+  //       ]);
 
-      // Check for updates immediately on app start if enabled
-      if (autoUpdateEnabled && !previousCheckUpdates) {
-        AutoUpdaterModule.checkForUpdates();
-      }
-    } catch (err) {
-      console.error('Failed to initialize auto-updater:', err);
-    }
+  //       // Check for updates immediately on app start if enabled
+  //       if (autoUpdateEnabled && !previousCheckUpdates) {
+  //         AutoUpdaterModule.checkForUpdates();
+  //       }
+  //     } catch (err) {
+  //       console.error('Failed to initialize auto-updater:', err);
+  //     }
 
-    previousCheckUpdates = autoUpdateEnabled;
-  });
+  //     previousCheckUpdates = autoUpdateEnabled;
+  //   });
 }
