fc3a491add55e90947fca270df3c5ce70b5425fa
Jay Meistrich <jmeistrich@gmail.com>
2025-03-24 18:21:54 +0800

    Re-enable auto updates in app

--- src/systems/Updater.ts
+++ src/systems/Updater.ts
@@ -1,3 +1,7 @@
+import { AutoUpdaterModule } from '@/native-modules/AutoUpdater';
+import { settings$ } from '@/settings/SettingsFile';
+import { observe } from '@legendapp/state';
+
 export function initializeUpdater() {
   // Don't update in dev
   if (__DEV__) {
@@ -5,28 +9,28 @@ export function initializeUpdater() {
   }
 
   // TODO: Implement auto-updater
-  //   let previousCheckUpdates = false;
-  //   observe(async () => {
-  //     const autoUpdateEnabled = settings$.general.autoUpdate.enabled.get();
-  //     const checkInterval = settings$.general.autoUpdate.checkInterval.get() * 3600; // Convert hours to seconds
+  let previousCheckUpdates = false;
+  observe(async () => {
+    const autoUpdateEnabled = settings$.general.autoUpdate.enabled.get();
+    const checkInterval = settings$.general.autoUpdate.checkInterval.get() * 3600; // Convert hours to seconds
 
-  //     try {
-  //       // Configure auto-update based on settings
-  //       await Promise.all([
-  //         AutoUpdaterModule.setAutomaticallyChecksForUpdates(autoUpdateEnabled),
-  //         autoUpdateEnabled
-  //           ? AutoUpdaterModule.setUpdateCheckInterval(checkInterval)
-  //           : Promise.resolve(true),
-  //       ]);
+    try {
+      // Configure auto-update based on settings
+      await Promise.all([
+        AutoUpdaterModule.setAutomaticallyChecksForUpdates(autoUpdateEnabled),
+        autoUpdateEnabled
+          ? AutoUpdaterModule.setUpdateCheckInterval(checkInterval)
+          : Promise.resolve(true),
+      ]);
 
-  //       // Check for updates immediately on app start if enabled
-  //       if (autoUpdateEnabled && !previousCheckUpdates) {
-  //         AutoUpdaterModule.checkForUpdates();
-  //       }
-  //     } catch (err) {
-  //       console.error('Failed to initialize auto-updater:', err);
-  //     }
+      // Check for updates immediately on app start if enabled
+      if (autoUpdateEnabled && !previousCheckUpdates) {
+        AutoUpdaterModule.checkForUpdates();
+      }
+    } catch (err) {
+      console.error('Failed to initialize auto-updater:', err);
+    }
 
-  //     previousCheckUpdates = autoUpdateEnabled;
-  //   });
+    previousCheckUpdates = autoUpdateEnabled;
+  });
 }
